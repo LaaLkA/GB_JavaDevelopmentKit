2259a6bfcc8f3cacdcdb7b6dc4f266873a64a23c
LaaLkA <n.afonchenkov@yandex.ru>
2024-10-15 20:40:35 +0300

    Sem4 completed

--- src/main/java/Sems/Sem4/Ex3/Program.java
+++ src/main/java/Sems/Sem4/Ex3/Program.java
@@ -1,7 +1,7 @@
 package Sems.Sem4.Ex3;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Создайте телефонный справочник с помощью Map - телефон это ключ, а имя - это значение
@@ -10,10 +10,27 @@ import java.util.Map;
  */
 public class Program {
     public static void main(String[] args) {
-        Map<String,String> phonebook = new HashMap<>();
-        phonebook.put("8987", "Иван");
-        phonebook.put("+7-900-900-80-80", "Иван");
-        phonebook.put("+7-900-900-90-80", "Иван");
-        phonebook.put("+7-900-900-70-80", "Иван");
+        Map<String, String> phonebook = new HashMap<>();
+        phonebook.put("89871033770", "Ivan");
+        phonebook.put("89871033740", "Boris");
+        phonebook.put("89871033750", "Anna");
+        phonebook.put("89871033760", "superJulia");
+
+        System.out.println(findByMinProneNumber(phonebook));
+        System.out.println(findByMaxAlphabetName(phonebook));
+    }
+
+    public static String findByMinProneNumber(Map<String, String> phonebook) {
+        Set<Long> numbers = phonebook.keySet().stream().map(el -> Long.valueOf(el)).collect(Collectors.toSet());
+        long resNumber = numbers.stream().min(Comparator.naturalOrder()).get();
+        return String.format("Smallest number: %s, Owner of the phone number: %s",
+                resNumber, phonebook.get(String.valueOf(resNumber)));
+    }
+
+    public static String findByMaxAlphabetName(Map<String, String> phonebook) {
+        Set<Map.Entry<String, String>> elements = phonebook.entrySet();
+        Map.Entry<String, String> name = elements.stream().max((entry1, entry2) ->
+                entry1.getValue().compareTo(entry2.getValue())).get();
+        return String.format("Max alphabet name is: %s", name.getValue());
     }
 }
