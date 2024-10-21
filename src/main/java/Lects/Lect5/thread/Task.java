9b93cd8f5a47db849121ef9656fe718170a51f2c
LaaLkA <n.afonchenkov@yandex.ru>
2024-10-21 16:56:11 +0300

    Lect5 in progress

--- src/main/java/Lects/Lect5/thread/Task.java
+++ src/main/java/Lects/Lect5/thread/Task.java
@@ -1,25 +1,30 @@
 package Lects.Lect5.thread;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task implements Runnable {
-    private int value;
+    private final AtomicInteger value;
     private CountDownLatch cdl;
 
     public Task(int value) {
-        this.value = value;
+        this.value = new AtomicInteger(value);
     }
 
     public void setCdl(CountDownLatch cdl) {
         this.cdl = cdl;
     }
 
-    public void inc(){
-        value++;
+    public void inc() {
+//        synchronized (Task.class){
+//            value++;
+//        }
+        value.incrementAndGet();
     }
 
     public int getValue() {
-        return value;
+//        return value;
+        return value.intValue();
     }
 
     @Override
--- src/main/java/Lects/Lect5/thread/TicTac.java
+++ src/main/java/Lects/Lect5/thread/TicTac.java
@@ -2,19 +2,25 @@ package Lects.Lect5.thread;
 
 public class TicTac implements Runnable {
     private String bracket;
+    private final Object monitor;
 
     public TicTac(String bracket) {
         this.bracket = bracket;
+        this.monitor = TicTac.class;
     }
 
     @Override
     public void run() {
         while (true) {
-            System.out.print(bracket);
-            try {
-                Thread.sleep(300);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            synchronized (monitor) {
+                System.out.print(bracket);
+                try {
+                    Thread.sleep(500);
+                    monitor.notify();
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
